16399eed169035ed0a480c80425f83c35cb192e9
Sean Sullivan <dev@seanmedia.ca>
2020-01-21 21:09:35 +0000

    Write functions for calculating the area of geometric shapes.

--- hello.js
+++ hello.js
@@ -2,6 +2,19 @@ const sayHello = function (name) {
   console.log("Hello, " + name);
 }
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
