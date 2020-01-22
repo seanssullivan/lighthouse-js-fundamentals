c845071d4b9437d51d86b2f3d6325a8455cf3bb6
Sean Sullivan <dev@seanmedia.ca>
2020-01-22 14:39:06 +0000

    Write a function to return the last index of a value in an array.

---
+++ the-final-index.js
@@ -0,0 +1,15 @@
+function lastIndexOf(arr, value) {
+  itemIndex = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == value) {
+      itemIndex = i;
+    }
+  }
+  return itemIndex;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
